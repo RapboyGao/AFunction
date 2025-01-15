ccee5a96ebb7018e5cadee2ce6e94e0ca8474aa7
RapboyGao <atgao74201@qq.com>
2025-01-15 16:00:34 +0800

    2025.01.15 重构 AFunctionOneExampleView 和 AValueElementArrayView，添加精度和返回单位支持，更新视图以使用新结构

--- Sources/AFunction/Views/AFunctionOneExampleView.swift
+++ Sources/AFunction/Views/AFunctionOneExampleView.swift
@@ -1,3 +1,4 @@
+import AUnit
 import AValue
 import AViewUI
 import SwiftUI
@@ -7,6 +8,8 @@ public struct AFunctionOneExampleView: View {
     var name: String
     var instance: ([AValue]) throws -> AValue
     var args: AFunction.ExampleArgs
+    var precision: NumberFormatStyleConfiguration.Precision
+    var returningUnit: AUnit?
 
     var result: AValue? {
         try? instance(args.values)
@@ -16,21 +19,33 @@ public struct AFunctionOneExampleView: View {
         AWrappingStack {
             Text(name + "(")
             ForEach(args.valueElements) { value in
-                Text(verbatim: value.value.description)
+                AValueElementArrayView(value: value, precision: precision)
+            }
+            Text(") = ")
+            if let result = result {
+                AValueAsArgumentView(value: result, precision: precision, unit: returningUnit)
+            } else {
+                Text("?")
             }
         }
     }
 
-    public init(name: String, _ values: [AValue], arguments: AFunction.Arguments, instance: @escaping ([AValue]) throws -> AValue) {
+    public init(name: String, _ values: [AValue], arguments: AFunction.Arguments, precision: NumberFormatStyleConfiguration.Precision, unitOfReturn: AUnit?, instance: @escaping ([AValue]) throws -> AValue) {
         self.name = name
         self.instance = instance
         self.args = AFunction.ExampleArgs(values, arguments: arguments)
+        self.precision = precision
+        self.returningUnit = unitOfReturn
+    }
+
+    public init(func function: AFunction, precision: NumberFormatStyleConfiguration.Precision, exampleIndex: Int) {
+        self.init(name: function.shortName, function.examples[exampleIndex].values, arguments: function.arguments, precision: precision, unitOfReturn: function.returnValue.unit, instance: function.instance)
     }
 }
 
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
 #Preview {
     List {
-        AFunctionOneExampleView(name: AFunction.cosFunction.shortName, AFunction.cosFunction.examples[0].values, arguments: AFunction.cosFunction.arguments, instance: AFunction.cosFunction.instance)
+        AFunctionOneExampleView(func: .pointDistance, precision: .fractionLength(0 ... 5), exampleIndex: 1)
     }
 }
--- Sources/AFunction/Views/AValueElementArrayView.swift
+++ Sources/AFunction/Views/AValueElementArrayView.swift
@@ -1,3 +1,4 @@
+import AUnit
 import AValue
 import AViewUI
 import SwiftUI
@@ -5,8 +6,8 @@ import SwiftUI
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
 public struct AValueElementArrayView: View {
     public var value: AValueArrayElement
-
-    public var precision: FloatingPointFormatStyle.Configuration.Precision
+    public var precision: NumberFormatStyleConfiguration.Precision
+    @State private var selectedUnit: AUnit?
 
     public var format: FloatingPointFormatStyle<Double> {
         .number.grouping(.never).precision(precision)
@@ -16,32 +17,22 @@ public struct AValueElementArrayView: View {
         if !value.isFirst {
             Text(", ")
         }
-        switch value.value {
-        case .number(let double):
-            if let unit = value.unit {
-                Menu {
-                    ForEach(unit.unitType.allUnits) { thisUnit in
-                        if let someValue = unit.convert(value: double, to: thisUnit) {
-                            Text(someValue, format: format) + Text(" ") + Text(thisUnit.symbol)
-                        } else {
-                            Text("- ") + Text(thisUnit.symbol)
-                        }
-                    }
-                } label: {
-                    Text(double, format: format) + Text(unit.symbol)
-                }
+        AValueAsArgumentView(value: value.value, precision: precision, unit: value.unit)
+    }
 
-            } else {
-                Text(double, format: format)
-            }
-        case .point(let x, let y):
-            Text("(") + Text(x, format: format) + Text(",") + Text(y, format: format) + Text(")")
-        case .location(let latitude, let longitude):
-            Text(latitude, format: ALatitudeFormat.dM(digits: 1)) + Text(longitude, format: ALongitudeFormat.dM(digits: 1))
-        case .boolean(let bool):
-            Toggle("", isOn: .constant(bool)).labelsHidden()
-        case .string, .groundWind, .minutes, .calendar, .dateDifference:
-            Text(value.value.description)
-        }
+    public init(value: AValueArrayElement, precision: NumberFormatStyleConfiguration.Precision) {
+        self.value = value
+        self.precision = precision
+    }
+}
+
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+#Preview {
+    List {
+        AValueElementArrayView(value: .init(id: 1, value: 35, isLast: false, unit: .meters), precision: .fractionLength(0 ... 5))
+        AValueElementArrayView(value: .init(id: 0, value: .point(x: 2, y: 5), isLast: true, unit: .meters), precision: .fractionLength(0 ... 5))
+        AValueElementArrayView(value: .init(id: 0, value: .location(latitude: 40.126447, longitude: 113.15691), isLast: true, unit: .meters), precision: .fractionLength(0 ... 5))
+        AValueElementArrayView(value: .init(id: 0, value: true, isLast: true, unit: .meters), precision: .fractionLength(0 ... 5))
+        AValueElementArrayView(value: .init(id: 0, value: .groundWind(limit: .b737), isLast: true, unit: .metersPerSecond), precision: .fractionLength(0 ... 5))
     }
 }
