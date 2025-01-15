dd6d1a0b861dfbb8ad7723999eafce98b13d858c
RapboyGao <atgao74201@qq.com>
2025-01-15 14:07:52 +0800

    2025.01.15 重构 AFunction 相关结构，删除旧示例参数，添加新的示例参数结构，更新 AFunction 以支持新参数格式

--- Sources/AFunction/Models/AFunction.swift
+++ Sources/AFunction/Models/AFunction.swift
@@ -2,12 +2,19 @@ import AValue
 import Foundation
 
 public struct AFunction: Sendable, Identifiable, CustomStringConvertible {
+    /// 唯一标识符
     public let id: Int
+    /// 简短名称
     public let shortName: String
+    /// 参数列表
     public let arguments: Arguments
+    /// 返回值
     public let returnValue: Argument
+    /// 所属部分
     public let part: Part
+    /// 示例参数列表
     public let examples: [ExampleArgs]
+    /// 实例函数
     public let instance: @Sendable ([AValue]) throws -> AValue
 
     public init(id: Int, shortName: String, arguments: Arguments, returnValue: Argument, part: Part, examples: [ExampleArgs], instance: @escaping @Sendable ([AValue]) throws -> AValue) {
